0453ba2def08db48cd63ebd3b516d7aa0ea5cc5a
Dias <dias.aubakirov6@gmail.com>
2026-07-23 21:42:17 +0500

    fix(start): fix greeting message logic for new users

--- src/raw_inputs/raw_inputs_handler.py
+++ src/raw_inputs/raw_inputs_handler.py
@@ -13,22 +13,31 @@ router = Router(name="raw_inputs_handler")
 @router.message(CommandStart())
 async def bot_start(message: Message, services: ServiceConteiner):
 
-    user_scheme = UserCreate(
-        telegram_id=message.from_user.id,
-        username=message.from_user.username,
-        first_name=message.from_user.first_name
-    )
-
     try: 
-        user = await services.users.create_user(user_scheme)
+        user = await services.users.get_user_by_telegram_id(message.from_user.id)
 
-        if not user:
+        if user:
             await message.answer(
                 f"С возвращением, {message.from_user.first_name}! 👋\n\n"
                 "Я готов продолжать работу. Отправляй свои идеи и заметки!"
             )
             return
+        
+        user_scheme = UserCreate(
+                telegram_id=message.from_user.id,
+                username=message.from_user.username,
+                first_name=message.from_user.first_name
+            )
 
+        await services.users.create_user(user_scheme)
+        await message.answer(
+                f"👋 Привет, {message.from_user.username}!\n\n"
+                "Я помогу сохранять твои идеи, заметки и планы.\n"
+                "Просто отправь мне текст или голосовое сообщение, "
+                "а я структурирую его и сохраню."
+            )
+        return
+        
     except Exception as e: 
         print("=== ПОЛНЫЙ ЛОГ ОШИБКИ ===")
         traceback.print_exc()
@@ -38,13 +47,6 @@ async def bot_start(message: Message, services: ServiceConteiner):
             "Наши сервера немного устали. Пожалуйста, попробуй команду /start через пару минут!"
         )
         return
-    
-    await message.answer(
-        f"👋 Привет, {message.from_user.username}!\n\n"
-        "Я помогу сохранять твои идеи, заметки и планы.\n"
-        "Просто отправь мне текст или голосовое сообщение, "
-        "а я структурирую его и сохраню."
-    )
     
 @router.message(F.text)
 async def create_raw_input(message: Message, services: ServiceConteiner):
